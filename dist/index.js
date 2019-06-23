95f8a36e16614301a005d38d00c13e8b42002bc9
Mike McGrath <mmcgrath@mindedge.com>
2019-06-23 08:15:14 -0400

    updates

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Greeter = function (name) { return "Hello " + name; };
+exports.Greeter = (name) => `Hello ${name}`;
