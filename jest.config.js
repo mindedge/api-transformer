c7d95aa15ed6287eacc5d65a3d9cc29c1983789b
Mike McGrath <mmcgrath@mindedge.com>
2019-06-23 08:56:48 -0400

    add code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+    "transform": {
+      "^.+\\.jsx?$": "babel-jest", 
+      "^.+\\.tsx?$": "ts-jest"
+      
+    },
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    "moduleFileExtensions": ["ts", "tsx", "js", "jsx", "json", "node"],
+    "collectCoverage":true,
+    "coverageDirectory": "./coverage/",
+    "coverageReporters":["text", "lcov", "clover"]
 };
